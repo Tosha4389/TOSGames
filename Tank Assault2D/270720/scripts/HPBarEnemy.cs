933f6c87856b3b3834491348984945755f506380
Антон Орлов <9024849@mail.ru>
2020-07-28 00:20:33 +0300

    бэк 270720

---
+++ Tank Assault2D/270720/scripts/HPBarEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBarEnemy : HPBar
+{
+    public MainAI enemyMain;    
+
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        TrackingBar();
+    }
+
+    public override void ScaleBar()
+    {
+        if(enemyMain.enemyHP < 100 && enemyMain.enemyHP >= 0) {
+            greenBar.SetActive(true);
+            redBar.SetActive(true);
+        }
+        scaleBar = greenBar.transform.localScale;
+        greenBar.transform.localScale = new Vector3(enemyMain.enemyHP / 100f, scaleBar.y, scaleBar.z);
+        greenBar.transform.localPosition = new Vector3((1f - enemyMain.enemyHP / 100f) / -2f, 0f, 0f);
+
+    }
+
+    public override void TrackingBar()
+    {
+        transform.position = enemyMain.transform.position + Vector3.up * 1.5f;
+        
+    }
+
+}
---
+++ Tank Assault2D/270720/scripts/MoveAI.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 90
+
+public class MoveAI : MonoBehaviour
+{
+    [Header("Вручную")]
+    public float offsetY = 2f;
+    public float radius = 1f;
+    public float speed = 0.1f;
+    public float timeStop = 1f;
+
+
+    [Header("Автоматически")]
+    public Vector3 pointFinish;
+    public new Rigidbody rigidbody;
+    public BorderCheck borderCheck;
+    public Vector3 distancePoint;
+    public Vector3 distanceTarget;
+    public Vector3 tankPosition;
+
+    private float dTime;
+    private Vector2 pointX, pointY;
+    private float move;
+    private IEnumerator coroutine;
+    private float xLimit, yLimit;
+
+
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        dTime = Time.fixedDeltaTime;
+        borderCheck = GetComponent<BorderCheck>();
+
+
+    }
+
+    void Start()
+    {        
+        pointFinish = new Vector3(0f, 0f, rigidbody.position.z);
+        coroutine = Stop(timeStop);
+        StartCoroutine(coroutine);
+    }
+
+    void FixedUpdate()
+    {
+        Move();        
+        if(Tank.S)
+            tankPosition = Tank.S.transform.position;
+        else return;
+        
+    }
+
+    public void Move()
+    {
+        pointX.x = rigidbody.position.x - 3f;
+        pointX.y = rigidbody.position.x - 1f;
+
+        pointY.x = rigidbody.position.y - 3f;
+        pointY.y = rigidbody.position.y - 1f;
+
+        Stop(1f);
+
+        distancePoint = pointFinish - rigidbody.position;
+        distanceTarget = tankPosition - rigidbody.position;
+
+        xLimit = Mathf.Clamp(transform.position.x, -borderCheck.screenWidth + borderCheck.size, borderCheck.screenWidth - borderCheck.size);
+        yLimit = Mathf.Clamp(transform.position.y, 0f + borderCheck.size, transform.position.y);
+        transform.position = new Vector3(xLimit, yLimit, transform.position.z);
+
+        if(distanceTarget.sqrMagnitude < 30f && rigidbody.velocity.y == 0f) {            
+            
+        }
+
+        if(distanceTarget.sqrMagnitude >= 30f) {
+            move = -speed;
+            rigidbody.AddForce(pointFinish.normalized * dTime * move, mode: ForceMode.VelocityChange);
+            rigidbody.transform.up = pointFinish;
+        }
+
+
+
+
+
+
+    }
+
+    private IEnumerator Stop(float waitTime)
+    {
+        while(true) {
+            
+            pointFinish.x = Random.Range(pointX.x, pointX.y);
+            pointFinish.y = Random.Range(pointY.y, pointY.x);
+            pointFinish = new Vector3(pointFinish.x, pointFinish.y, 0f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+}
