0d8686eecfbce47570d2d5599e02bdd0bb9f0ea1
Антон Орлов <9024849@mail.ru>
2020-08-22 00:00:52 +0300

    bac_210820
    НавМеш2Д

---
+++ Tank Assault2D/210820/Scripts/Managers/LoadParametersTank.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//20
+
+public class LoadParametersTank : MonoBehaviour
+{
+    static public LoadParametersTank S;
+
+    [Header("Автоматически")]
+    public int playerDamageLoad;
+    public float fireRateLoad;
+    public float scatterLoad;
+    public float accuracyLoad;
+    public float speedMoveLoad;
+    public float speedRotateLoad;
+    public float speedRotateTurretLoad;
+    public Vector3 gunPos;
+    public Sprite[] tankSprites = new Sprite[3];
+
+    private void Awake()
+    {
+        if(S == null) {
+            S = this;
+        } else {            
+            Destroy(gameObject);
+        }        
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
---
+++ Tank Assault2D/210820/Scripts/Test/Test2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Test2 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        var agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        agent.destination = new Vector3(-4f, 9f, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
